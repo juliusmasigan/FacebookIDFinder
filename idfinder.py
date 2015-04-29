1afa67de44a141cdfb117ec9ec11279bae4957a8
Julius Masigan <juliusmasigan@gmail.com>
2015-04-30 07:56:42 +0800

    Added a method for getting the profile ID.
    Signed-off-by: Julius Masigan <juliusmasigan@gmail.com>

--- idfinder.py
+++ idfinder.py
@@ -7,7 +7,7 @@ from prettyprint import pp
 
 class IDFinder:
 
-    API = 'https://graph.facebook.com/v2.3';
+    API = 'https://graph.facebook.com/v2.3'
     APP_ID = '369253336592554'
     APP_SECRET = '142a28496a7f69457c993638ddc58e3e'
     ACCESS_TOKEN = 'CAAFP1X31sKoBABusvrvhMHtFTCLxGNreGwWwNZAC2VtZBItYHGfPdZATfqG33t3pXk34dB55' + \
@@ -17,35 +17,47 @@ class IDFinder:
     #Constructor
     def __init__(self):
         warnings.filterwarnings('ignore')
-        self.checkToken()
+        #self.checkToken()
 
     def checkToken(self):
         payload = {'access_token':self.ACCESS_TOKEN}
         request = requests.get(self.API+'/me', params=payload)
         response = request.json()
-
-        if (response['error']):
+        if ('error' in response):
             print response['error']['message']
             self.getToken()
 
     def getToken(self):
         IDFinder.ACCESS_TOKEN = raw_input('Please enter new token: \n')
 
+    def setSourceFile(self, filename):
+        self.srcfilename = filename
+		
+    def getProfileID(self, name):
+	payload = {'access_token':IDFinder.ACCESS_TOKEN, 'q':name, 'type':'user', 'limit':10}
+	request = requests.get(self.API+'/search', params=payload)
+	response = request.json()
+	if('error' in response):
+	    print response['error']['message']
+	    self.getToken()
+	    self.getProfileID(name)
+
     @staticmethod
     def printToken():
         print IDFinder.ACCESS_TOKEN
 
-    def setSourceFile(self, filename):
-        self.srcfilename = filename
-        fhandle = open(self.srcfilename, 'r+')
-        
-        line = fhandle.readline()
-        while line:
-            print line
-            line = fhandle.readline()
-        fhandle.close()
 
 
 a = IDFinder()
-filename = raw_input('File: \n')
-a.setSourceFile(filename)
+a.setSourceFile(sys.argv[1])
+
+f = open(a.srcfilename, 'r+')
+line = f.readline()
+while line:
+	lsplit = line.split(',', 2)
+	del lsplit[2]
+	name = ' '
+	name = name.join(lsplit)
+	a.getProfileID(name)
+	line = f.readline()
+f.close()
